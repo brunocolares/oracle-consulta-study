c109295cf7e05ad2ef8681c3c35f1ad0fd174f11
bruno <brunocolaresdev@gmail.com>
2025-01-17 21:46:34 -0300

    alias, null, aritmetica

--- select.sql
+++ select.sql
@@ -13,3 +13,34 @@ FROM Jobs;
 select location_id, locations.* 
 from locations;
 
+SELECT DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID
+FROM DEPARTMENTS;
+
+-- select com expressão aritmética
+select first_name, last_name, salary, salary * 1.15
+FROM employees;
+
+select first_name, last_name, salary, (salary + 100) * 1.15
+FROM employees;
+
+select * from employees;
+
+-- NULL
+select first_name, last_name, job_id, salary, commission_pct
+from employees;
+
+select first_name, last_name, job_id, commission_pct, 200000 * commission_pct
+FROM employees
+WHERE commission_pct IS NULL;
+
+-- ALIAS
+SELECT first_name AS nome,
+       last_name sobrenome,
+       salary salario
+FROM employees;
+
+select first_name "Nome", last_name "Sobrenome", salary "Salário ($)", commission_pct "Percentual de comissão"
+from employees;
+
+-- Operador de concatenação
+
