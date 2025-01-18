5e3725753d3b9126d08c941a107987475c3e8dc2
bruno <brunocolaresdev@gmail.com>
2025-01-17 21:26:11 -0300

    criação do arquivo de consulta sql

---
+++ select.sql
@@ -0,0 +1,15 @@
+-- Describe
+DESCRIBE employees;
+DESC regions;
+DESC COUNTRIES;
+
+-- Select
+SELECT * FROM countries;
+SELECT * FROM Jobs;
+
+SELECT JOB_ID, JOB_TITLE
+FROM Jobs;
+
+select location_id, locations.* 
+from locations;
+
