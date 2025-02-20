11d7dc70b00de8afe97e820202342ef0bf2830dd
bruno <brunocolaresdev@gmail.com>
2025-02-19 23:00:15 -0300

    revisão de manipulação de data

---
+++ revisao-1.sql
@@ -0,0 +1,21 @@
+/* Revisão de conteúdos vistos até 19/02/2025 */
+
+-- 1
+SELECT TO_CHAR(sysdate, 'DD/MM/YYYY HH24:MI:SS') DATA_ATUAL,
+    TO_CHAR(SYSDATE + 7, 'DD/MM/YYYY HH24:MI:SS') PROXIMA_SEMANA
+FROM dual;
+
+-- 2
+SELECT first_name || ' ' || last_name "Nome completo",
+    TO_CHAR(hire_date, 'FMDAY DD/MM/YYYY') "Data de contratação"
+FROM employees
+WHERE TO_CHAR(hire_date, 'YYYY') = TO_CHAR(sysdate, 'YYYY');
+
+-- 3
+SELECT first_name,
+    TRUNC(sysdate - hire_date) as DIAS_TRABALHADOS
+FROM EMPLOYEES
+ORDER BY DIAS_TRABALHADOS DESC;
+
+-- 4
+
